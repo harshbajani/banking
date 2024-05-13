2ac7d0efe2dfef40d17fefa590cd4f8d232b91c7
Harshbajani <47421975+harshbajani@users.noreply.github.com>
2024-05-13 10:39:35 +0530

    Update page.tsx

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
 import AuthForm from "@/components/AuthForm";
 import React from "react";
 
-const SignIn = () => {
+const SignUp = () => {
   return (
     <section className="flex-center size-full max-sm:px-6">
-      <AuthForm type="sign-in" />
+      <AuthForm type="sign-up" />
     </section>
   );
-=======
-import React from "react";
-
-const SignIn = () => {
-  return <div>SignIn</div>;
->>>>>>> b4c6fdeb46799950362b72adbac9038f85d518ea
 };
-
-export default SignIn;
